4cb0372a049d13f825959cf7a568bc2483e6a79f
Dvwhite81 <123031200+Dvwhite81@users.noreply.github.com>
2023-12-11 08:36:34 -0500

    Initial commit

---
+++ src/components/PhaseOne/PhaseOne.jsx
@@ -0,0 +1,11 @@
+import './PhaseOne.css';
+
+function PhaseOne({ greeting }) {
+  return (
+    <div id='phase-one-container'>
+      <h1>Hello {greeting}</h1>
+    </div>
+  );
+}
+
+export default PhaseOne;
